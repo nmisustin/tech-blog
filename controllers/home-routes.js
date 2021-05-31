f5b2a6f2cabceaef16aca698d7e7c123f900e35f
nmisustin <nmisustin@gmail.com>
2021-05-31 18:22:08 -0400

    added single post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,5 +45,47 @@ router.get('/login', (req, res) => {
         return;
     }
     res.render('login')
+});
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id 
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(data => {
+            if(!data){
+                res.status(404).json({message: 'No post with this id found'})
+                return;
+            }
+            const post = data.get({plain: true});
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 })
 module.exports = router;
