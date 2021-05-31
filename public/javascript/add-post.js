0509c628fe43e6cab2886433b2a620cce754b744
nmisustin <nmisustin@gmail.com>
2021-05-31 14:11:29 -0400

    added the dashboard routes and frontend js

---
+++ public/javascript/add-post.js
@@ -0,0 +1,25 @@
+async function newPostHandler(event){
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const post_content = document.querySelector('#post-content').value.trim();
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_content
+        }),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
+    if(response.ok){
+        document.location.replace('/dashboard')
+    }
+    else{
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newPostHandler);
